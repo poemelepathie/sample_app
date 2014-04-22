d3d682eb06abf8e2dad59d9c801d3314ffd0e32c
Bart Roodnat <broodnat@elmar.nl>
2014-04-22 14:13:33 +0200

    Spec to features

--- Gemfile
+++ Gemfile
@@ -4,11 +4,11 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.4'
 gem 'bootstrap-sass', '2.3.2.0'
-gem 'sprockets', '2.11.0'
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
+  gem 'spork-rails', '4.0.0'
 end
 
 group :test do
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-    
-    it "should have the h1 'Sample App'" do      
+
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
@@ -21,8 +21,8 @@ describe "Static pages" do
   end
 
   describe "Help page" do
-    
-    it "should have the h1 'Help'" do      
+
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
@@ -34,7 +34,7 @@ describe "Static pages" do
   end
 
   describe "About page" do
-    
+
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
@@ -47,7 +47,7 @@ describe "Static pages" do
   end
 
   describe "Contact page" do
-    
+
     it "should have the h1 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('h1', :text => 'Contact')
